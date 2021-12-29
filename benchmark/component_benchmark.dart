d7bc57ccdf8834c0ba676f8e811b7e8f7d645317
Jochum van der Ploeg <jochum@vdploeg.net>
2021-12-29 18:45:33 +0100

    Added a benchmark that showcases the problem

---
+++ benchmark/component_benchmark.dart
@@ -0,0 +1,79 @@
+import 'package:oxygen/oxygen.dart';
+import 'package:benchmark/benchmark.dart';
+
+class Test100Component extends Component<void> {
+  @override
+  void init([void data]) {}
+
+  @override
+  void reset() {}
+}
+
+class Test50Component extends Component<void> {
+  @override
+  void init([void data]) {}
+
+  @override
+  void reset() {}
+}
+
+void main() {
+  group('Component', () {
+    group('With 100000 entities', () {
+      World? world;
+
+      QueryManager? queryManager;
+
+      Query? query100;
+      Query? query50;
+
+      setUp(() {
+        world = World();
+        world!.registerComponent(() => Test100Component());
+        world!.registerComponent(() => Test50Component());
+
+        for (var i = 0; i < 100000; i++) {
+          final entity = world!.createEntity();
+
+          entity.add<Test100Component, void>();
+          if (i % 2 == 0) {
+            entity.add<Test50Component, void>();
+          }
+        }
+        queryManager = QueryManager(world!.entityManager);
+
+        query100 = queryManager?.createQuery([Has<Test100Component>()]);
+        query50 = queryManager?.createQuery([Has<Test50Component>()]);
+      });
+
+      tearDown(() {
+        world = null;
+        queryManager = null;
+      });
+
+      group('100%', () {
+        benchmark('Iterate over 100% entities without getting', () {
+          for (final entity in query100!.entities) {}
+        });
+
+        benchmark('Iterate over 100% entities with getting', () {
+          for (final entity in query100!.entities) {
+            entity.get<Test100Component>();
+          }
+        });
+      });
+
+      group('50%', () {
+        benchmark('Iterate over 50% entities without getting', () {
+          for (final entity in query50!.entities) {}
+        });
+
+        benchmark('Iterate over 50% entities with getting', () {
+          for (final entity in query50!.entities) {
+            entity.get<Test50Component>();
+          }
+        });
+      });
+    });
+  });
+}
