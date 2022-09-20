feda4905c5fc2b97152aae0dbe41687559c5f785
Lukas Klingsbo <SEFUNCTeamRatatosk@skandia.se>
2022-09-20 14:48:49 +0200

    Remove assert from object pool

--- lib/src/pooling/object_pool.dart
+++ lib/src/pooling/object_pool.dart
@@ -31,10 +31,6 @@ abstract class ObjectPool<T extends PoolObject<V>, V> {
       expand((_count * 0.2).floor() + 1);
     }
     final object = _pool.removeLast();
-    assert(
-      data == null,
-      '$T expects an instance of $V but received null',
-    );
     return object..init(data);
   }
 
